6f9c9c1eea1b5b12bcf50d270d4a1f561f526fab
Thomas Papiernik <tom.papiernik@gmail.com>
2021-09-02 13:28:30 -0500

    Updated challenge_logic.js

--- static/js/challenge_logic.js
+++ static/js/challenge_logic.js
@@ -149,7 +149,7 @@ legend.onAdd = function() {
   }
 
   // 3. Use d3.json to make a call to get our Tectonic Plate geoJSON data.
-  d3.json("static/js/PB2002_boundaries.json").then(function(data) {
+  d3.json("https://tpapiernik.github.io/Module_13_Challenge/static/js/PB2002_boundaries.json").then(function(data) {
     console.log(data);
     // Creating a GeoJSON layer with the retrieved data.
     L.geoJson(data, {
